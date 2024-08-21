05de4c67ba3b1668b8d46cef6087892c5ce74521
CaoThiNhuY <nhuycao25@gmail.com>
2024-08-21 20:01:32 +0700

    Add files via upload

---
+++ bai5 so tuoi.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 21 19:55:41 2024
+
+@author: nhuy23733881
+"""
+nam_sinh=(int(input("Nhap nam sinh =")))
+nam_hien_nay=(int(input("Nhap nam hien nay =")))
+so_tuoi=nam_hien_nay - nam_sinh
+print("Tuoi cua ban= ", so_tuoi)
