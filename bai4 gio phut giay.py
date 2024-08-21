7ce6f600b0fa05556210945ebcedeee0ef6cda06
CaoThiNhuY <nhuycao25@gmail.com>
2024-08-21 19:55:34 +0700

    Add files via upload

---
+++ bai4 gio phut giay.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 21 19:47:23 2024
+
+@author: nhuy23733881
+"""
+gio=int(input("Nhap gio= "))
+phut=int(input("Nhap phut= "))
+giay=int(input("Nhap giay= "))
+tong_giay= gio*60*60 + phut*60 + giay
+print("tong_giay= ",tong_giay)
