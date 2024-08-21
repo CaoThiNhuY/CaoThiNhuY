8a28e17fa92e566b2976fbfd2a7fc7677cec373e
CaoThiNhuY <nhuycao25@gmail.com>
2024-08-21 19:25:57 +0700

    Add files via upload

---
+++ trungbinhcong.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 21 19:09:25 2024
+
+@author: nhuy23733881
+"""
+import math
+a=float(input("Nhap a="))
+b=float(input("Nhap b="))
+c=float(input("Nhap c="))
+d=float(input("Nhap d="))
+tbc = (a+b+c+d)/4
+print("Trung binh cong la:",tbc)
