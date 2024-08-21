3cccdd9b54c8cd54dc659dfffb0e3ab52685824c
CaoThiNhuY <nhuycao25@gmail.com>
2024-08-21 19:47:12 +0700

    Add files via upload

---
+++ bai3tongchuso.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 21 19:31:08 2024
+
+@author: nhuy23733881
+"""
+
+import math
+print("Nhap cac chu so")
+N=(int(input("Nhap so nguyen duong N=")))
+tong=(N//10)+(N%10)
+print("Tong cac chu so la:", tong)
