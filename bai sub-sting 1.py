e776121f89bd36f37d9fc2100305ee287062e839
CaoThiNhuY <nhuycao25@gmail.com>
2024-08-22 19:22:53 +0700

    Add files via upload

---
+++ bai sub-sting 1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 18:40:50 2024
+
+@author: Student
+"""
+
+#1
+print(' Đại học Quốc gia \n Khu phố 6 \n P. Linh Trung \n Q. Thủ Đức \n Tp. HCM')
+#2
+chuoi=("Đại học Quốc gia, Khu phố 6, P.Linh Trung, Q.Thủ Đức, Tp.HCM")
+print(chuoi.replace(',','\n').replace('P.','').replace('Q.','').replace('Tp.',''))
